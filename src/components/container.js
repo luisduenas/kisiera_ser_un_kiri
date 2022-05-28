113031866c9f224d2226f08d3d5ae8598c5f23b3
luisduenas <luis@octahedroid.com>
2022-05-27 22:58:22 -0700

    fix: background image

--- src/components/container.js
+++ src/components/container.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Container({ children }) {
   return (
-    <div className={`container mx-auto px-5 bg-huellitas bg-fixed`}>
+    <div className={`container mx-auto px-5 bg-huellitas bg-contain bg-fixed`}>
       {children}
     </div>
   );
